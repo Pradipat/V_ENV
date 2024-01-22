96633e3c8de07b03bf884c2d223176dc4400c878
Pradipat <P0861719933@gmail.com>
2024-01-22 15:46:26 +0700

    Create django views

--- general/views.py
+++ general/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def ecommerce_index_view(request):
+    '''This function render index page of ecomerce views'''
+
+    return HttpResponse('Welcome to CN334 Pradipat views!')
